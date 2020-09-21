732ef46340cdf32b82193c300d1fa01d55ef6da9
LittlePinkCookie <alouache.loan@gmail.com>
2020-09-21 08:28:41 +0200

    clean git

--- src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/IPresenter.java
+++
@@ -1,9 +0,0 @@
-package fr.ensicaen.genielogiciel.mvp.presenter;
-
-public interface IPresenter {
-    String getInput();
-
-    void setPresenter( LoginPresenter presenter );
-
-    void setMessage( String text );
-}
