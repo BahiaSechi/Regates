5ecceace441c10c15db680ef0a8869e0c8fec7b1
rich <rich@ecole.ensicaen.fr>
2020-09-28 10:18:23 +0200

    Setup lombok

--- src/main/java/regates/mvp/model/Coordinate.java
+++ src/main/java/regates/mvp/model/Coordinate.java
@@ -1,4 +1,28 @@
 package regates.mvp.model;
 
-public class Coordinate {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.awt.geom.Point2D;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordinate extends Point2D {
+    @Override
+    public double getX() {
+        return 0;
+    }
+
+    @Override
+    public double getY() {
+        return 0;
+    }
+
+
+    @Override
+    public void setLocation(double x, double y) {
+
+    }
+    //Create a vector
+
 }
