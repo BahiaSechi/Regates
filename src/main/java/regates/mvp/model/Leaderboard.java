adc2de74c470fb1527da65d8a313b4e91640da43
elemazier <elise.le-mazier@ecole.ensicaen.fr>
2020-10-01 09:20:06 +0200

    ajout gestion score

--- src/main/java/regates/mvp/model/Leaderboard.java
+++ src/main/java/regates/mvp/model/Leaderboard.java
@@ -3,10 +3,16 @@ package regates.mvp.model;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Leaderboard {
 
-
+    private List<Score> scores = new ArrayList<>();
     private Leaderboard instance;
 
     public Leaderboard getInstance() {
@@ -15,17 +21,28 @@ public class Leaderboard {
         return instance;
     }
 
-    public void sortByDate() {
+    public void readScore() {
 
+        try {
+            Scanner scanner = new Scanner(new File("../../../resources/regates/mvp/scoresData.txt"));
+            while(scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                String[] buffer = new String[3];
+                buffer = line.split(";");
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+                scores.add(new Score(Float.parseFloat(buffer[0]), formatter.parse(buffer[1]), buffer[1]));
+            }
+        } catch (FileNotFoundException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void sortByName() {
-
+    public void sortByDate() {
+        Collections.sort(scores, Score.ComparatorDate);
     }
 
     public void sortByScore() {
-
+        Collections.sort(scores, Score.ComparatorScore);
     }
-
-
 }
--- src/main/java/regates/mvp/model/Score.java
+++ src/main/java/regates/mvp/model/Score.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Comparator;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -14,5 +15,20 @@ public class Score {
     private float value;
     private Date date;
     private String player;
+
+
+    public static Comparator<Score> ComparatorDate = new Comparator<Score>() {
+        @Override
+        public int compare(Score o1, Score o2) {
+            return o1.getDate().compareTo(o2.getDate());
+        }
+    };
+
+    public static Comparator<Score> ComparatorScore = new Comparator<Score>() {
+        @Override
+        public int compare(Score o1, Score o2) {
+            return Float.compare(o1.getValue(), o2.getValue());
+        }
+    };
 
 }
