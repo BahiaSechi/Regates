c1f482d0960757d63b2dc0bb33f87eb4370675df
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-10-01 18:35:14 +0200

    Manfred me crie alors je push Open new windows pour jouer au jeu

--- src/main/java/regates/mvp/presenter/MenuController.java
+++ src/main/java/regates/mvp/presenter/MenuController.java
@@ -1,19 +1,45 @@
 package regates.mvp.presenter;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuController {
 
-
-    public void handleHoverIn(MouseEvent mouseEvent) {
+    public void handleExit(MouseEvent mouseEvent) {
+        //TODO le refaire plus propre, on a deja une fonnction exit dans le boardcontroller ????
+        Platform.exit();
     }
 
-    public void handleHoverOut(MouseEvent mouseEvent) {
+    public void chooseMap(MouseEvent mouseEvent) {
+    }
+
+    public void playGame(MouseEvent mouseEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/regates/mvp/BoardView.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Regate game");
+            stage.setScene(new Scene(root1));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }    }
+
+    public void displayLeader(MouseEvent mouseEvent) {
     }
 }
