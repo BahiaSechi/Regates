9bfe7178af6bcfa87be4e4f2942744e9ac0f470c
Loan Alouache <loan.alouache@ecole.ensicaen.fr>
2020-10-01 02:00:01 +0200

    Fix tests

--- src/test/java/regates/mvp/model/BoatSpeedTest.java
+++ src/test/java/regates/mvp/model/BoatSpeedTest.java
@@ -8,15 +8,8 @@ import static org.junit.Assert.assertThrows;
 public class BoatSpeedTest {
     @Test
     public void should_return_first_boat_speed() throws Exception {
-        Boat b = new Boat(1, 1, null);
-        float speed = b.determinateSpeed(1, 1);
+        Boat b = new Boat(1, null);
+        double speed = b.determinateSpeed(1);
         assertEquals(speed, 1.7, 0.01);
-    }
-
-    @Test
-    public void should_throw_exception_on_invalid_angle() {
-        Boat b = new Boat(1, 1, null);
-        assertThrows(Exception.class, () -> b.determinateSpeed(1, -1));
-        assertThrows(Exception.class, () -> b.determinateSpeed(1, 181));
     }
 }
