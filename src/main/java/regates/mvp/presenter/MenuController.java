201e868fc6cbe5cc9d8a91b9a1a85543e7c62ae4
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-10-05 09:56:42 +0200

    Ajout MapView manque Leaderboard et beaucoup de style

--- src/main/java/regates/mvp/presenter/MenuController.java
+++ src/main/java/regates/mvp/presenter/MenuController.java
@@ -1,44 +1,59 @@
 package regates.mvp.presenter;
 
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class MenuController {
 
     public void handleExit(MouseEvent mouseEvent) {
-        //TODO le refaire plus propre, on a deja une fonnction exit dans le boardcontroller ????
         Platform.exit();
     }
 
     public void chooseMap(MouseEvent mouseEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/regates/mvp/MapView.fxml"));
+            Parent root2 = (Parent) fxmlLoader.load();
+            Scene scene = new Scene(root2, 1310, 983);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Regate game");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.requestFocus();
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void playGame(MouseEvent mouseEvent) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/regates/mvp/BoardView.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
+            BoardController bc = fxmlLoader.getController();
+            Scene scene = new Scene(root1, 1310, 983);
+
+            bc.setScene(scene);
+
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setTitle("Regate game");
-            stage.setScene(new Scene(root1));
+            stage.setScene(scene);
             stage.setResizable(false);
+            stage.requestFocus();
             stage.show();
+            stage.setOnCloseRequest((event)-> bc.exitGame());
         } catch (IOException e) {
             e.printStackTrace();
-        }    }
+        }
+    }
 
     public void displayLeader(MouseEvent mouseEvent) {
     }
