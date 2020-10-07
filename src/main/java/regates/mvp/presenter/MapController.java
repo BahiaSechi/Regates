e5a99ebae81540056ba2a3638dedb2c7a6500608
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-10-07 12:14:52 +0200

    Cleanup view

--- src/main/java/regates/mvp/presenter/MapController.java
+++ src/main/java/regates/mvp/presenter/MapController.java
@@ -1,27 +1,20 @@
 package regates.mvp.presenter;
 
-import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 public class MapController {
 
     private Stage stage;
 
-    /**
-     * When you click on the custom ImageView, you can load a file.
-     * @param mouseEvent
-     */
-    public void customClick(MouseEvent mouseEvent) {
+    public void customClick() {
         FileChooser fileChooser = new FileChooser();
-        File selectedFile = fileChooser.showOpenDialog(stage);
+        fileChooser.showOpenDialog(stage);
     }
 
     /**
      * Function to get the stage we are in.
-     * @param stage
+     * @param stage Stage we are in.
      */
     public void setStage(Stage stage) {
         this.stage = stage;
